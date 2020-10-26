8befc982d74783b9fb86f29902c087b7c3670231
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 20:07:58 +0000

    create seed data to test migrations

---
+++ sinatra_exercise_app/db/seeds.rb
@@ -0,0 +1,4 @@
+#seed data
+tesa = User.create(username: "Tesa")
+Exercise.create(name: "barbell deadlift", duration: 20, type: "weight training")
+Exercise.create(name: "treadmill running", duration: 15, type: "cardio")
