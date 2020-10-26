1c7aa6e632fbd3592b3d53e63593d6ec143dffbf
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 23:25:12 +0000

    make changes to form in login view page

--- sinatra_exercise_app/app/controllers/application_controller.rb
+++ sinatra_exercise_app/app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/login' do
-    binding.pry
+    #binding.pry
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
   
-  post '/login' do
+  post '/signup' do
     @user = User.new(:username => params[:username], :password => params[:password])
 		
 		if @user.save
