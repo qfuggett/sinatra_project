ebeccd3ed2e1892bce89d0fe6016e7a5e4b69432
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 21:11:32 +0000

    update type argument inside .create methods to detail

--- sinatra_exercise_app/db/seeds.rb
+++ sinatra_exercise_app/db/seeds.rb
@@ -1,4 +1,4 @@
 #seed data
 tesa = User.create(username: "Tesa")
-Exercise.create(name: "barbell deadlift", duration: 20, type: "weight training")
-Exercise.create(name: "treadmill running", duration: 15, type: "cardio")
+Exercise.create(name: "barbell deadlift", duration: 20, detail: "weight training")
+Exercise.create(name: "treadmill running", duration: 15, detail: "cardio")
