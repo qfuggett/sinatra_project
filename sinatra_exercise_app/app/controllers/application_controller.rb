c841088907856c08660db070589f8d5bbf8e82d8
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 21:28:46 +0000

    create post route in application controller

--- sinatra_exercise_app/app/controllers/application_controller.rb
+++ sinatra_exercise_app/app/controllers/application_controller.rb
@@ -17,7 +17,13 @@ class ApplicationController < Sinatra::Base
   
   post '/login' do
     binding.pry
-    
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/user"
+    else
+      redirect to "/failure"
+    end
   end
 
 end
