c8de192077653229de25cfe62f708c871f8fb376
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 21:32:57 +0000

    enable sessions and add helpers method in application controller

--- sinatra_exercise_app/app/controllers/application_controller.rb
+++ sinatra_exercise_app/app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
@@ -16,13 +18,23 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/login' do
-    binding.pry
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to "/user"
     else
       redirect to "/failure"
+    end
+  end
+  
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
     end
   end
 
