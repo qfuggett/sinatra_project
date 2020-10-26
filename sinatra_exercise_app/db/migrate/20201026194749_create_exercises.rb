91a06f9c6967c8d269540dc4074c2f0e2772ebe1
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-26 22:41:16 +0000

    update login and error view pages

--- sinatra_exercise_app/db/migrate/20201026194749_create_exercises.rb
+++ sinatra_exercise_app/db/migrate/20201026194749_create_exercises.rb
@@ -4,6 +4,7 @@ class CreateExercises < ActiveRecord::Migration
       t.string :name
       t.integer :duration
       t.string :detail
+      t.datetime :exercise_time
       t.string :user_id
     end
   end
