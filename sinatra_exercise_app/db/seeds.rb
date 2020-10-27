d0aeaa463061670a8187fe708ce0a4b752bd5ba6
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-27 18:00:24 +0000

    update seeds file and updated users index view page

--- sinatra_exercise_app/db/seeds.rb
+++ sinatra_exercise_app/db/seeds.rb
@@ -1,4 +1,4 @@
 #seed data
-tesa = User.create(username: "tesa", password: "test")
+tesa = User.create(username: "tesa", email: "tesa@test.com", password: "test")
 Exercise.create(name: "barbell deadlift", duration: 20, detail: "weight training")
 Exercise.create(name: "treadmill running", duration: 15, detail: "cardio")
