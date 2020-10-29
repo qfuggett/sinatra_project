f8683a385260aedcceb6ad211dc526729ecd590f
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-29 16:25:42 +0000

    made changes to exercises index view page and exercises controller

--- sinatra_exercise_app/app/controllers/exercises_controller.rb
+++ sinatra_exercise_app/app/controllers/exercises_controller.rb
@@ -23,13 +23,13 @@ class ExercisesController < ApplicationController
   get '/exercises/:id' do
     binding.pry
     
-    @exercise = Exercise.find(params[:id])
+    @exercise = Exercise.find_by(id: params["id"])
     erb :'exercises/show'
   end
   
    get '/exercises/:id/edit' do
 
-    @exercise = Exercise.find(params[:id])
+    @exercise = Exercise.find_by(id: params["id"])
     erb :'exercises/edit'
   end
   
