a0c524a9f5f187004464281e67dafe412c88d1c8
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-31 22:18:55 +0000

    remove comments from exercise controller

--- sinatra_exercise_app/app/controllers/exercises_controller.rb
+++ sinatra_exercise_app/app/controllers/exercises_controller.rb
@@ -15,44 +15,38 @@ class ExercisesController < ApplicationController
   end
   
   get '/exercises/:id' do
-    #check if logged in, send back to index
     if logged_in?
       @exercise = current_user.exercises.find_by(id: params["id"])
-    #add if not found, send back to index
       erb :'exercises/show'
-    else redirect "/exercises/index"
+      else redirect "/exercises"
     end
   end
   
    get '/exercises/:id/edit' do
-        #check if logged in, send back to index
     if logged_in?
       @exercise = current_user.exercises.find_by(id: params["id"])
       erb :'exercises/edit'
-    else redirect "/exercises/index"
+      else redirect "/exercises"
     end
   end
   
   patch '/exercises/:id' do
-        #check if logged in, send back to index
     if logged_in?
       @exercise = current_user.exercises.find_by(id: params["id"])
       @exercise.update(name: params["name"], duration: params["duration"], detail: params["detail"])
       
       redirect "/exercises/#{@exercise.id}"
-    else redirect "/exercises/index"
+    else redirect "/exercises"
     end
   end
   
   delete '/exercises/:id/delete' do
-        #check if logged in, send back to index
     if logged_in?
       @exercise = current_user.exercises.find_by(id: params["id"])
       @exercise.destroy
       
       redirect "/exercises"
-      
-    else redirect "/exercises/index"
+    else redirect "/exercises"
     end
   end
 end
